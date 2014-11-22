b182667838c07a59b02ad598d16177514fcd26f9
Me 226291 <226291@popcatdev.nfshost.com>
2014-11-22 04:35:17 +0000

    added basic nav bar and error log area

--- js/navController.js
+++ js/navController.js
@@ -1,4 +1,21 @@
 storganManley.controller('navController', function($scope) {
+	$scope.title = "Storgan Manley";
+	$scope.showError = false;
+	$scope.error = "~ not set: error should not be showing ~";
+
+	$scope.homeBtn = function(){
+		$scope.error = "clicked home";
+		$scope.showError = true;
+	};
+	$scope.chartBtn = function(){
+		$scope.error = "clicked chart";
+		$scope.showError = true;
+	};
+
+	$scope.submitBtn = function(){
+		$scope.error = "clicked submit";
+		$scope.showError = true;
+	};
 
 
 });
